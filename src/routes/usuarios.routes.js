ea460b070670512f49771c9fee39a5abe0f7c089
Leonardo Oliveira <leonardodeoliveira.tds1.senai@gmail.com>
2024-10-24 11:38:48 -0300

    Feat: Create get all user

--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -1,7 +1,14 @@
 import { Router } from "express";
 
-const usariosRoutes = Router();
+const usuariosRoutes = Router();
 
-let ususarios = []
+let usuarios = []
 
-export default usariosRoutes;
+usuariosRoutes.get("/", (req, res) => {
+    return res.status(200).json({
+        message: usuarios.length == 0 ? "Não há usuários cadastrados" : `Total
+        de usuários: ${usuarios.length, usuarios}`
+    })
+})
+
+export default usuariosRoutes;
